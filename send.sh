8f88dffc3d6f0fb5f444cf05f28952a4ed88efd8
Jonathan Bastnagel <jonthebagel@gmail.com>
2019-03-11 01:05:37 -0500

    Update send.sh

--- send.sh
+++ send.sh
@@ -76,5 +76,5 @@ WEBHOOK_DATA='{
 
 echo "$WEBHOOK_DATA"
 
-(curl --fail --progress-bar -A "GitLabCI-Webhook" -H Content-Type:application/json -H X-Author:n3bs#8383 -d "$WEBHOOK_DATA" "$2" \
+(curl --fail --progress-bar -A "GitLabCI-Webhook" -H Content-Type:application/json -H X-Author:n3bs#8097 -d "$WEBHOOK_DATA" "$2" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
